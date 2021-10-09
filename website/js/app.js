f9625379a36d174a6afe5b8664f02d1913299de0
hosamsaleh1 <mr.hosamsaleh@yahoo.com>
2021-10-09 02:57:58 +0200

    Editing js & css

--- website/js/app.js
+++ website/js/app.js
@@ -5,11 +5,11 @@ const description = document.getElementById('description')
 const imageUrl = document.getElementById('imageUrl')
 const showCard = document.getElementById('showCard')
 
-const card = "<div class="card" style="width: 18rem;">
+const card = `<div class="card" style="width: 18rem;">
 <img src="{{imageUrl}}" class="card-img-top" alt="...">
 <div class="card-body">
   <h5 class="card-title">{{title}}</h5>
   <p class="card-text">{{description}}</p>
-  <a href="#" class="btn btn-primary">Read More</a>
+  <a href="news" class="btn btn-primary">Read More</a>
 </div>
-</div>"
+</div>`
